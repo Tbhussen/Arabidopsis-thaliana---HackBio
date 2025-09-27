2c9af4cc5e9490d48ad562a23085d93c064180a3
Tbhussen <tamim.b.hussein@gmail.com>
2025-09-27 10:18:55 +0300

    Update Transcriptome_count.sh

--- Transcriptome_count.sh
+++ Transcriptome_count.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Download annotation file
-#wget https://ftp.ensemblgenomes.ebi.ac.uk/pub/plants/release-62/gff3/arabidopsis_thaliana/Arabidopsis_thaliana.TAIR10.62.gff3.gz
+wget https://ftp.ensemblgenomes.ebi.ac.uk/pub/plants/release-62/gff3/arabidopsis_thaliana/Arabidopsis_thaliana.TAIR10.62.gff3.gz
 
 #unzip the gff3 file
-#gunzip Arabidopsis_thaliana.TAIR10.62.gff3.gz
+gunzip Arabidopsis_thaliana.TAIR10.62.gff3.gz
 
 #rename the gff3 file to c_elegans.gff3
 # this is the file that will be used for featureCounts
-#mv Arabidopsis_thaliana.TAIR10.62.gff3 A_thaliana.gff3
+mv Arabidopsis_thaliana.TAIR10.62.gff3 A_thaliana.gff3
 
 #run featureCounts
 # -O: count reads overlapping multiple features
@@ -17,10 +17,10 @@
 # -a: annotation file (GFF3)
 # -o: output file
 
-#mkdir -p counts
+mkdir -p counts
 
 echo Apply Feature Counts to the BAM files
 
 featureCounts -O -t gene -g ID -a genome/A_thaliana.gff3 -o counts/counts.txt IGV/*.bam
 
-cd counts/
+cd counts/ || exit
